5c283f53af5fed5c3f842d2cdc2ab32856adb487
EC2 Default User <20yuteo@gmail.com>
2019-07-05 12:31:18 +0000

    making_layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  
   def top
   end
+  
+  def login
+  end
+  
+  def signup
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  
+  get '/recipe', to: 'recipe#index'
+
+  get '/user', to: 'user#index'
+
   get '/home', to: 'home#top'
+  
+  get '/login', to: 'home#login'
+  
+  get '/signup', to: 'home#signup'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
